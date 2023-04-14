3ba464dd8a76b131264cc726c69200805e8b2a3e
novikov-al-st <novikov.al.st@gmail.com>
2023-04-14 06:45:52 +0300

    feat: extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+  switch (str) {
+    case null:
+      str = 'null';
+      break;
+    case false:
+      str = 'false';
+      break;
+    default:
+      break;
+  }
+  
+  switch (options.addition) {
+    case null:
+      options.addition = 'null';
+      break;
+    case false:
+      options.addition = 'false';
+      break;
+    default:
+      break;
+  }
+  
+  if(!str) {
+    return '';
+  }
+
+  return Array(options.repeatTimes).
+              fill( str + 
+                 repeater(options.addition ? options.addition : '', {
+                    repeatTimes: options.additionRepeatTimes, 
+                    separator: options.additionSeparator,
+                    additionContext: true
+                 })
+         ).join( options.separator || (options.additionContext ? '|' : '+'));
 }
+
 
 module.exports = {
   repeater
